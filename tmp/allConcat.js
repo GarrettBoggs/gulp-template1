fd492d3a0186bb85ba7a79d7f69430e4bd0fecad
Andrew Fisher and Garrett Boggs <andrew@>
2016-10-11 10:25:36 -0700

    does some stuff

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -25,15 +25,18 @@ $(document).ready(function(){
   $("#time").text(moment());
 });
 
-var apiKey = "3a48ec39ff16da970bbf5a1726b81ba7";
+var Weather = require('./../js/weather.js').weatherModule;
+
+var displayHumidity = function(city, humidityData) {
+  $('.showWeather').text("The humidity in " + city + " is " + humidityData + "%");
+}
 
 $(document).ready(function() {
+  var currentWeatherObject = new Weather();
   $('#weatherLocation').click(function() {
     var city = $('#location').val();
     $('#location').val("");
-    $('.showWeather').text("The city you have chosen is " + city + ".");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=' + apiKey, function(response){
-      $('.showWeather').text('The humidity in ' + city + " is  "+response.main.humidity +"% give or take 20%...or more");
+    var humidity = currentWeatherObject.getWeather(city, displayHumidity);
     });
+    console.log("Notice: The GET request has been made.");
   });
-});
